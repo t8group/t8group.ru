01a7702ce13faa587fbab8291e3abf72be21c0d0
ftroitsky <fedor.troitsky@gmail.com>
2014-09-08 22:17:39 +0400

    people section — this closes #9

--- templates/mailer.php
+++ templates/mailer.php
@@ -4,7 +4,7 @@
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Get the form fields and remove whitespace.
         $name = strip_tags(trim($_POST["name"]));
-				$name = str_replace(array("\r","\n"),array(" "," "),$name);
+		$name = str_replace(array("\r","\n"),array(" "," "),$name);
         $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
         $message = trim($_POST["message"]);
 
